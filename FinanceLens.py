3873c97da601d4353436cf293b1d4353f16754af
raddev9 <raghavendraakshayy.d99@gmail.com>
2025-11-14 16:06:19 -0600

    Add financial tips section to enhance user guidance on money management

--- FinanceLens.py
+++ FinanceLens.py
@@ -69,6 +69,7 @@ if st.session_state.app_started:
 
 
     #The code below creates the section Budget Tracking which is designed to track your budget on a base level
+    
     elif section == ("Budget Tracking 💰"):
 
 
@@ -96,3 +97,29 @@ if st.session_state.app_started:
         st.write("This investment after just 5 years will give you a profit of: $" + str(profit))
         st.write("With the remaining 60 percent of your balance, which is $" + str(sixty) + ", put the money in your bank account.")
         st.write("This allows you to build up your savings portfolio as well as gradually increase your return through both your bank as well as the S&P 500.")
+
+
+    #This section provides financial tips for the user
+
+    else:
+        st.header("Financial Tips:")
+        st.write("Get tips and insights on how to manage and deal with money better!")
+        st.write("1. It's always good to diversify your investment portfolio, many people just leave their savings in the bank, but a better way could be by put a percentage of money in the bank while leaving some in other areas such as the S&P 500.")
+        st.write("")
+        st.write("2. Personal finance isn't hard, but people make it much more harder then they need it to be. Many individuals don't have any way to track their money but there are many tools online in order to do so, this app being an example.")
+        st.write("")
+        st.write("3. Learning about taxes and other policies early can help you plan future expenses such as college much better, enabling you to excel in managing your personal finances.")
+        st.write("")
+        st.write("4. Index funds such as the S&P 500 can be a good way to see how the market itself is doing in a particular time frame as it is essentially a compilation of 500 different companies.")
+        st.write("")
+        st.write("5. Many leading tech giants recommend people to read about the economy through various articles and subjects in order to gain insights on various financial topics to better understand the market and the economy itself.")
+        st.write("")
+        st.write("6. Using oppurtunities such as compound interest, investments, and etcetera, building long term wealth is just as important as earning money.")
+        st.write("")
+        st.write("7. Many wealthy people recommend putting most of an individuals money into investments rather then leaving it as cash.")
+        st.write("")
+        st.write("8. Although many investments do give returns, there is a risk factor in many of them an example being in the stock market and real estate where prices are inconsistent and can change, so other investment options such as the traditional or Roth IRA could be beneficial.")
+        st.write("")
+        st.write("9. Many factors can affect stock prices and/or real estate prices; for example, if a company was moving its headquarters to a new city, the surrounding areas real estate could increase in price, similarily if a company were to announce a new product, the stock price of the company could go up.")
+        st.write("")
+        st.write("10. It is helpful to start thinking about retirement(if applicable) even at an early age as it helps plan out finances along the way or establishes a somewhat unpolished but established direction an individual is going towards in their personal finances.")
